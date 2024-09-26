6391d1ffb36c0de24a08a37395b5e3831fe6075b
blgriwzow <107067628+blgriwzow@users.noreply.github.com>
2024-09-26 16:40:27 -0700

    Update cis129_lab03_coffeShop.py
    Updating variable names, and including variables to hold calculations (taxAmount, itemsTotal). Great job!

--- cis129_lab03_coffeShop.py
+++ cis129_lab03_coffeShop.py
@@ -1,27 +1,29 @@
 print('***************************************')
 print('My Coffee, Muffin, and More Shop')
 print('Number of coffees bought?')
-value1=int(input())
+numCoffees=int(input())
 print('Number of muffins bought?')
-value2=int(input())
+value2=int(input()) # variable name could be more descriptive
 print('Number of scones bought?')
-value3=int(input())
+value3=int(input()) # variable name could be more descriptive
 print('Number of teas bought?')
-value4=int(input())
+value4=int(input()) # variable name could be more descriptive
 print('***************************************')
 print('***************************************')
 print('My Coffee, Muffin, and More Shop Receipt')
-coffee=value1*5
-print(str(value1)+ " Coffee at $5 each: $ " + str(coffee)+ ".00")
+coffee=numCoffees*5
+print(str(numCoffees)+ " Coffee at $5 each: $ " + str(coffee)+ ".00")
 muffin=value2*4
 print(str(value2)+ " Muffins at $4 each: $ " + str(muffin)+ ".00")
 scone=value3*3
 print(str(value3)+ " Scones at $3 each: $ " + str(scone)+ ".00")
 tea=value4*2
 print(str(value4)+ " Tea at $2 each: $ " + str(tea)+ ".00")
-((coffee+muffin+scone+tea)*0.06)
-print("6% tax: " + str((coffee+muffin+scone+tea)*0.06))
+itemsTotal = (coffee+muffin+scone+tea)
+taxAmount=(itemsTotal*0.06)
+print("6% tax: " + str(taxAmount))
 print('---------')
-print("Total: " + str(((coffee+muffin+scone+tea)+((coffee+muffin+scone+tea)*0.06))))
+print("Total: " + str(itemsTotal+taxAmount))
 print('***************************************')
 print('Thank You For Choosing "My Coffee, Muffins, and More Shop"! We Hope To See You Again!')
+#Great job! - Brittany
